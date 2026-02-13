38fabe3c81c5cbb881b52823ffcb00c8bfe7dc9f
DaehoDaeho <relayer@naver.com>
2026-02-13 17:13:59 +0900

    .

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,38 +8,35 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rigidBody;
 
+    private Vector2 inputVec;   // 입력받은 방향을 저장할 변수.
+
     private void Awake()
     {
         moveSpeed = 5.0f;
         rigidBody = GetComponent<Rigidbody2D>();
-        Debug.Log("PlayerController Awake 호출!!");
     }
 
-    private void OnEnable()
+    /// <summary>
+    /// New Input System의 Move 액션으로부터 메시지를 받는 이벤트 함수.
+    /// </summary>
+    /// <param name="value">입력 정보를 담고 있는 클래스</param>
+    void OnMove(InputValue value)
     {
-        Debug.Log("PlayerController OnEnable 호출!!");
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("PlayerController Start 호출!!");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape) == true)
-        {
-            Debug.Log("Update : ESC 입력!!!");
-        }
+        // 입력 값을 Vector2 타입으로 읽어와서 저장.
+        inputVec = value.Get<Vector2>();
+        Debug.Log("입력 방향 : " + inputVec);
     }
 
     private void FixedUpdate()
     {
         if(rigidBody != null)
         {
-            rigidBody.linearVelocity = new Vector2(moveSpeed, 0.0f);
+            // 다음에 이동할 방향 벡터 계산.
+            // 입력값 * 이동 속도 * 고정 프레임 시간.
+            Vector2 nextVec = inputVec * moveSpeed * Time.fixedDeltaTime;
+
+            // 현재 위치에 이동량을 더해서 이동.
+            rigidBody.MovePosition(rigidBody.position + nextVec);
         }
     }
 }
