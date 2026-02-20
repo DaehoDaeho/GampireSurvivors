16e02cedc7c4a092f11e46136c35c28f9025b076
JJH0204 <>
2026-02-20 17:45:51 +0900

    .

--- Assets/Scripts/Map/InfiniteMap.cs
+++ Assets/Scripts/Map/InfiniteMap.cs
@@ -30,7 +30,7 @@ public class InfiniteMap : MonoBehaviour
 
         // 거리에 따른 재배치 로직.
         // x축 거리가 y축 거리보다 멀면 x축 방향으로 이동.
-        if(diffX > diffY)
+        if (diffX > diffY)
         {
             // Transform.Translate : 오브젝트를 이동시켜주는 함수.
             // 타일의 크기가 20 * 20이라고 가정할 때, 방향에 따라 40만큼 이동 (3 * 3 구조 기준)
@@ -39,6 +39,11 @@ public class InfiniteMap : MonoBehaviour
         else if(diffX < diffY)  // Y축 거리가 X축 거리보다 멀다면 Y축 방향으로 이동.
         {
             transform.Translate(Vector3.up * dirY * 40.0f);
+        }
+        else if(diffX == diffY)
+        {
+            Vector3 move = new Vector3(dirX, dirY, 0.0f);
+            transform.Translate(move * 40.0f);
         }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    private Animator animator;
+
     private Rigidbody2D rigidBody;
+
+    private SpriteRenderer spriteRenderer;
 
     public Vector2 inputVec;   // 입력받은 방향을 저장할 변수.
 
@@ -14,6 +18,8 @@ public class PlayerController : MonoBehaviour
     {
         moveSpeed = 5.0f;
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     /// <summary>
@@ -37,6 +43,25 @@ public class PlayerController : MonoBehaviour
 
             // 현재 위치에 이동량을 더해서 이동.
             rigidBody.MovePosition(rigidBody.position + nextVec);
+        }
+    }
+
+    void LateUpdate()
+    {
+        // 방향 전환.
+        if (inputVec.x != 0.0f)
+        {
+            spriteRenderer.flipX = inputVec.x < 0.0f;   // 조건이 만족하면 true, 아니면 false를 대입.
+        }
+
+        if(animator != null)
+        {
+            // 벡터의 길이의 제곱.
+            // inputVec.magnitude ; 벡터의 정확한 길이. 반드시 길이가 필요한 경우에만 쓴다.
+            float move = inputVec.sqrMagnitude; // 벡터의 크기의 제곱을 알아오는 코드.
+                                                // '입력 유무'나 '크기 비교'를 할 때 사용.
+
+            animator.SetFloat("Speed", move);
         }
     }
 }
